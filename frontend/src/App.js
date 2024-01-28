03722dc2e5e91746c0513976e4174f9753494d37
SumanChaudhary <sumanchaudhary724@gmail.com>
2024-01-28 22:38:37 +1100

    added mapbox and isntalled react map gl

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,19 @@
+import * as React from "react";
+import Map from "react-map-gl";
+
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <Map
+      mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+      initialViewState={{
+        longitude: -122.4,
+        latitude: 37.8,
+        zoom: 14,
+      }}
+      style={{ width: 600, height: 400 }}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+    />
+  );
 }
 
 export default App;

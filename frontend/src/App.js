f378191598c5026b8d5eed4b3da1d94d87388cdf
SumanChaudhary <sumanchaudhary724@gmail.com>
2024-02-03 21:40:23 +1100

    updated zoom value

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ function App() {
       initialViewState={{
         longitude: -122.4,
         latitude: 37.8,
-        zoom: 12,
+        zoom: 14,
       }}
       style={{ width: 600, height: 400 }}
       mapStyle="mapbox://styles/mapbox/streets-v9"
